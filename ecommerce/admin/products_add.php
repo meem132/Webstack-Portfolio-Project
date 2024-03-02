0e518806a30f168c8a24d7bbeb85061ea84c2f8e
EvansOdhams <evansodhams@gmail.com>
2024-03-02 17:07:06 +0300

    PHP code for adding a new product to the database

--- ecommerce/admin/products_add.php
+++ ecommerce/admin/products_add.php
@@ -1,51 +1,44 @@
 <?php
-	include 'includes/session.php';
-	include 'includes/slugify.php';
+include 'includes/session.php';
+include 'includes/slugify.php';
 
-	if(isset($_POST['add'])){
-		$name = $_POST['name'];
-		$slug = slugify($name);
-		$category = $_POST['category'];
-		$price = $_POST['price'];
-		$description = $_POST['description'];
-		$filename = $_FILES['photo']['name'];
+if(isset($_POST['add'])){
+    $name = $_POST['name'];
+    $slug = slugify($name);
+    $category = $_POST['category'];
+    $price = $_POST['price'];
+    $description = $_POST['description'];
+    $filename = $_FILES['photo']['name'];
 
-		$conn = $pdo->open();
+    $conn = $pdo->open();
 
-		$stmt = $conn->prepare("SELECT *, COUNT(*) AS numrows FROM products WHERE slug=:slug");
-		$stmt->execute(['slug'=>$slug]);
-		$row = $stmt->fetch();
+    try {
+        $stmt = $conn->prepare("SELECT COUNT(*) AS numrows FROM products WHERE slug=:slug");
+        $stmt->execute(['slug'=>$slug]);
+        $row = $stmt->fetch();
 
-		if($row['numrows'] > 0){
-			$_SESSION['error'] = 'Product already exist';
-		}
-		else{
-			if(!empty($filename)){
-				$ext = pathinfo($filename, PATHINFO_EXTENSION);
-				$new_filename = $slug.'.'.$ext;
-				move_uploaded_file($_FILES['photo']['tmp_name'], '../images/'.$new_filename);	
-			}
-			else{
-				$new_filename = '';
-			}
+        if($row['numrows'] > 0){
+            $_SESSION['error'] = 'Product already exists';
+        } else {
+            $new_filename = '';
+            if(!empty($filename)){
+                $ext = pathinfo($filename, PATHINFO_EXTENSION);
+                $new_filename = $slug.'.'.$ext;
+                move_uploaded_file($_FILES['photo']['tmp_name'], '../images/'.$new_filename);    
+            }
 
-			try{
-				$stmt = $conn->prepare("INSERT INTO products (category_id, name, description, slug, price, photo) VALUES (:category, :name, :description, :slug, :price, :photo)");
-				$stmt->execute(['category'=>$category, 'name'=>$name, 'description'=>$description, 'slug'=>$slug, 'price'=>$price, 'photo'=>$new_filename]);
-				$_SESSION['success'] = 'User added successfully';
+            $stmt = $conn->prepare("INSERT INTO products (category_id, name, description, slug, price, photo) VALUES (:category, :name, :description, :slug, :price, :photo)");
+            $stmt->execute(['category'=>$category, 'name'=>$name, 'description'=>$description, 'slug'=>$slug, 'price'=>$price, 'photo'=>$new_filename]);
+            $_SESSION['success'] = 'Product added successfully';
+        }
+    } catch(PDOException $e) {
+        $_SESSION['error'] = $e->getMessage();
+    }
 
-			}
-			catch(PDOException $e){
-				$_SESSION['error'] = $e->getMessage();
-			}
-		}
+    $pdo->close();
+} else {
+    $_SESSION['error'] = 'Fill up the product form first';
+}
 
-		$pdo->close();
-	}
-	else{
-		$_SESSION['error'] = 'Fill up product form first';
-	}
-
-	header('location: products.php');
-
+header('location: products.php');
 ?>
