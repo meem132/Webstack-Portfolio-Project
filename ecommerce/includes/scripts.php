90c5108a70dc51d6366a79bbb8c0d00283b712ed
EvansOdhams <evansodhams@gmail.com>
2024-03-02 17:55:03 +0300

    Refactor JavaScript dependencies and custom scripts

--- ecommerce/includes/scripts.php
+++ ecommerce/includes/scripts.php
@@ -13,71 +13,71 @@
 <script src="dist/js/adminlte.min.js"></script>
 <!-- CK Editor -->
 <script src="bower_components/ckeditor/ckeditor.js"></script>
+<!-- Magnify -->
+<script src="magnify/magnify.min.js"></script>
+
 <script>
   $(function () {
-    // Datatable
-    $('#example1').DataTable()
-    //CK Editor
-    CKEDITOR.replace('editor1')
+    // DataTable initialization
+    $('#example1').DataTable();
+    // CK Editor initialization
+    CKEDITOR.replace('editor1');
+
+    // Magnify initialization
+    $('.zoom').magnify();
+
+    // Focus event for search input
+    $('#navbar-search-input').focus(function(){
+      $('#searchBtn').show();
+    });
+
+    // Focusout event for search input
+    $('#navbar-search-input').focusout(function(){
+      $('#searchBtn').hide();
+    });
+
+    // Submit event for product form
+    $('#productForm').submit(function(e){
+      e.preventDefault();
+      var product = $(this).serialize();
+      $.ajax({
+        type: 'POST',
+        url: 'cart_add.php',
+        data: product,
+        dataType: 'json',
+        success: function(response){
+          $('#callout').show();
+          $('.message').html(response.message);
+          if(response.error){
+            $('#callout').removeClass('callout-success').addClass('callout-danger');
+          }
+          else{
+            $('#callout').removeClass('callout-danger').addClass('callout-success');
+            getCart();
+          }
+        }
+      });
+    });
+
+    // Click event for close button
+    $(document).on('click', '.close', function(){
+      $('#callout').hide();
+    });
+
+    // Function to fetch cart items
+    function getCart(){
+      $.ajax({
+        type: 'POST',
+        url: 'cart_fetch.php',
+        dataType: 'json',
+        success: function(response){
+          $('#cart_menu').html(response.list);
+          $('.cart_count').html(response.count);
+        }
+      });
+    }
+
+    // Initial call to fetch cart items
+    getCart();
   });
-</script>
-<!--Magnify -->
-<script src="magnify/magnify.min.js"></script>
-<script>
-$(function(){
-	$('.zoom').magnify();
-});
-</script>
-<!-- Custom Scripts -->
-<script>
-$(function(){
-  $('#navbar-search-input').focus(function(){
-    $('#searchBtn').show();
-  });
-
-  $('#navbar-search-input').focusout(function(){
-    $('#searchBtn').hide();
-  });
-
-  getCart();
-
-  $('#productForm').submit(function(e){
-  	e.preventDefault();
-  	var product = $(this).serialize();
-  	$.ajax({
-  		type: 'POST',
-  		url: 'cart_add.php',
-  		data: product,
-  		dataType: 'json',
-  		success: function(response){
-  			$('#callout').show();
-  			$('.message').html(response.message);
-  			if(response.error){
-  				$('#callout').removeClass('callout-success').addClass('callout-danger');
-  			}
-  			else{
-				$('#callout').removeClass('callout-danger').addClass('callout-success');
-				getCart();
-  			}
-  		}
-  	});
-  });
-
-  $(document).on('click', '.close', function(){
-  	$('#callout').hide();
-  });
-
-});
-
-function getCart(){
-	$.ajax({
-		type: 'POST',
-		url: 'cart_fetch.php',
-		dataType: 'json',
-		success: function(response){
-			$('#cart_menu').html(response.list);
-			$('.cart_count').html(response.count);
-		}
-	});
-}
 </script>
