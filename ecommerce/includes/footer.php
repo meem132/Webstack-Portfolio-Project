9d2b3becfbb3b5e109ea6dd643568160ebe5b89c
EvansOdhams <evansodhams@gmail.com>
2024-03-02 17:50:41 +0300

    Footer Styling Information

--- ecommerce/includes/footer.php
+++ ecommerce/includes/footer.php
@@ -3,6 +3,6 @@
       <div class="pull-right hidden-xs">
         <b>All rights reserved</b>
       </div>
-      <strong>Copyright &copy; 2020 Brought to You By <a href="#">Evansites</a></strong>
+      <strong>Copyright &copy; 2024 Brought to You By <a href="#">Evansites</a></strong>
     </div>
 </footer>
